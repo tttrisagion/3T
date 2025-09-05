c43d7e672f18661a173f013b5caa48b541ef8f6c
Trisagion Developers <support@trisagion.xyz>
2025-09-05 20:19:24 +0000

    capturing production state and provide example for handling stale runs

--- celery-services/worker/reconciliation_engine.py
+++ celery-services/worker/reconciliation_engine.py
@@ -260,7 +260,8 @@ def get_desired_state(symbol: str) -> float:
                 FROM runs
                 WHERE exit_run = 0
                   AND end_time IS NULL
-                  AND live_pnl > 0
+                  AND live_pnl > 0.1
+                  AND abs(position_direction) > 0
                   AND symbol = %s
                   AND update_time >= NOW() - INTERVAL 10 MINUTE
                 HAVING total_pnl > 0 AND ABS(position) >= 1
